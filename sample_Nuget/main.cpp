91a95ff8c0c839bf58751313fe4a902325942530
kotaroyoshida1021 <y.kotaro.2091@gmail.com>
2022-01-26 21:52:58 +0900

    126-2152

--- sample_Nuget/main.cpp
+++ sample_Nuget/main.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 #include<GL/glut.h>
 
+#define MAXPOINTS 100
+GLint point[MAXPOINTS][2];
+int pointnum = 0;
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3d(1., 0., 0.);
-	glBegin(GL_QUAD_STRIP);
-	glVertex2d(-0.9, -0.9);
-	glVertex2d(0.9, -0.9);
-	glVertex2d(0.9, 0.9);
-	glVertex2d(-0.9, 0.9);
-	glEnd();
+	if (pointnum > 1) {
+		glColor3d(0.0, 0., 0.);
+		glBegin(GL_LINES);
+		for (int i = 0; i < pointnum; i++) {
+			glVertex2iv(point[i]);
+		}
+		glEnd();
+	}
 	glFlush();
 }
 
@@ -20,38 +24,37 @@ void initDisplay(void) {
 void resize(int w, int h) {
 	glViewport(0, 0, w, h);
 	glLoadIdentity();
-	glOrtho(-w / 200., w / 200., -h / 200.0, h / 200.0,-1.0,1.0);
+	//glOrtho(-w / 200., w / 200., -h / 200.0, h / 200.0,-1.0,1.0);
+	glOrtho(-0.5, (GLdouble)w - 0.5, (GLdouble)h - 0.5, -0.5, -1.0, 1.0);
 }
 
 void mouse(int button, int state, int x, int y) {
+	static int x0, y0;
 	switch (button)
 	{
 	case GLUT_LEFT_BUTTON://左クリック
-		printf("left");
+		point[pointnum][0] = x;
+		point[pointnum][1] = y;
+		if (state == GLUT_UP) {
+			glColor3d(0.0, 0.0, 0.0);
+			glBegin(GL_LINES);
+			glVertex2iv(point[pointnum-1]);
+			glVertex2iv(point[pointnum]);
+			glEnd();
+			glFlush();
+		}
+		else {
+		}
+		if (pointnum < MAXPOINTS - 1) pointnum++;
 		break;
 	case GLUT_RIGHT_BUTTON://右クリック
-		printf("right");
 		break;
 	case GLUT_MIDDLE_BUTTON:
-		printf("middle");
 		break;
 	default:
 		break;
 	}
-
-	printf("button is ");
-	switch (state) {
-	case GLUT_UP://押した→離したとき
-		printf("up");
-		break;
-	case GLUT_DOWN://押したとき
-		printf("down");
-		break;
-	default:
-		break;
-	}
-
-	printf(" at (%d %d) \n", x, y);
+	
 }
 int main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
